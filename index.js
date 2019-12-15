9e2b982316c6923cd4db538fa1b3f93a5c0b4fa2
AdamSiekierski <adam.siekiera@outlook.com>
2019-12-15 14:42:21 +0100

    Code quality changes

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const cowsay = require('cowsay')
-const chalk = require('chalk')
-const chance = require('chance').Chance();
-const fs = require('fs')
+#!/usr/bin/env node
 
-const proverbs = JSON.parse(fs.readFileSync(`${__dirname}/proverbs.json`, 'utf8'));
-const randomProverb = () => proverbs.proverbs[chance.integer({ min: 0, max: proverbs.proverbs.length - 1 })]
+const cowsay = require("cowsay")
+const chalk = require("chalk")
+const proverbs = require("./proverbs.json")
 
-const cowProgrammerProverb = () => {
-  console.log(
-    chalk.magenta(
-      cowsay.say({ text: randomProverb() })
-    )
-  )
-}
+const randomProverb = () =>
+  proverbs.proverbs[Math.floor(Math.random() * proverbs.proverbs.length)]
 
-module.exports = cowProgrammerProverb;
+console.log(chalk.magenta(cowsay.say({ text: randomProverb() })))
